78dbe43b2400ce00decaab6fd72954965390b451
MrsVit <mitvz@yandex.ru>
2025-09-24 21:19:32 +0300

    lab1

--- lab1_v14/src/main_lab1.cpp
+++ lab1_v14/src/main_lab1.cpp
@@ -4,9 +4,10 @@ int main(void){
     string input;
     char old_value;
     char new_value;
-    int n;
+    int a;
+    cout<<"Введите целевую строку";
     getline(cin, input);
-    cin>>old_value>>new_value>>n;
-    cout<<replaces(input, old_value, new_value, n);
-
+    cout<<"Что хотите заменить и на что хотите заменить?";
+    cin>>old_value>>new_value>>a;
+    cout<<replaces(input, old_value, new_value, a);
 }
